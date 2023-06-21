6bacb79f7430c3dea452750b5f4981451c4061c9
Rahul Bhati <rb083380@gmail.com>
2023-06-21 16:48:08 +0530

    creating file and folder

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import { CssBaseline } from '@mui/material';
+import { Route, Routes } from 'react-router-dom';
+import { Actors, Movies, MovieInformation, NavBar, Profile } from './index';
+import useStyles from './styles';
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div className={classes.root}>
+      <CssBaseline />
+      <NavBar />
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Routes>
+          <Route path="/" element={<Movies />} />
+          <Route path="/actors/:id" element={<Actors />} />
+          <Route path="/movie/:id" element={<MovieInformation />} />
+          <Route path="/profile/:id" element={<Profile />} />
+        </Routes>
+      </main>
     </div>
   );
 }
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function NavBar() {
+  console.log('navBar');
+  return (
+    <div>NavBar</div>
+  );
+}
+
+export default NavBar;
